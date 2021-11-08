2ef8154eb3a55b51c0f757710f4751e11c280e2e
Nes0x <66802265+Nes0x@users.noreply.github.com>
2021-11-08 16:15:49 +0100

    Update 2.0.0
    Dodano auto updater

--- src/main/java/pl/nesox/utils/Updater.java
+++ src/main/java/pl/nesox/utils/Updater.java
@@ -1,4 +1,59 @@
 package pl.nesox.utils;
-//soon
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import pl.nesox.TxtMaker;
+
+import javax.swing.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+
 public class Updater {
+
+
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    public Updater() throws IOException {
+        JSONObject json = readJsonFromUrl("https://txtmaker.cf/api/actual-version");
+
+        if (!json.getString("actualVersion").equalsIgnoreCase("2.0.0")) {
+            int result = JOptionPane.showConfirmDialog(
+                    TxtMaker.getFrame(),
+                    "Czy chcesz pobrać automatycznie do folderu nową wersje?",
+                    "Nowa wersja!",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE
+            );
+
+            if (result == JOptionPane.YES_OPTION) {
+                File file = new File(String.valueOf(Paths.get(".", "txtmaker_" + json.getString("actualVersion") + ".zip")));
+                URL url = new URL("https://txtmaker.cf" + json.getString("actualVersionDownloadLink"));
+                FileUtils.copyURLToFile(url, file);
+            }
+
+        }
+    }
+
 }
