08848f1162cb8031da3f3b8ea5708fc5dc7bbdf2
Nes0x <66802265+Nes0x@users.noreply.github.com>
2021-11-10 15:46:57 +0100

    Update 2.0.2

---
+++ src/main/TxtMaker.java
@@ -0,0 +1,115 @@
+package pl.nesox;
+
+import com.github.weisj.darklaf.LafManager;
+import com.github.weisj.darklaf.theme.OneDarkTheme;
+import pl.nesox.utils.ButtonManager;
+import pl.nesox.utils.DiscordIntegration;
+import pl.nesox.utils.Updater;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.prefs.Preferences;
+
+public class TxtMaker {
+    private static JFrame frame;
+    private static ButtonManager[] buttonsTxT;
+    public static Preferences variables = Preferences.userNodeForPackage(TxtMaker.class);
+
+    public static JFrame createFrame(int width, int high, String title, int action, boolean resizable) {
+        JFrame frame = new JFrame(title);
+        frame.setSize(width, high);
+        frame.setDefaultCloseOperation(action);
+        frame.setResizable(resizable);
+        frame.setLocationRelativeTo(null);
+        frame.setIconImage(
+                new ImageIcon(TxtMaker.class.getClassLoader().getResource("logo.png")).getImage()
+        );
+
+        return frame;
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        LafManager.install(new OneDarkTheme());
+
+
+        //tworzenie okna, zakładek, przycisków
+        frame = createFrame(600, 400, "TxtMaker", JFrame.EXIT_ON_CLOSE, false);
+
+        JTabbedPane main = new JTabbedPane();
+
+        JPanel[] panels = new JPanel[]{new JPanel(), new JPanel(), new JPanel()};
+        buttonsTxT = new ButtonManager[]{
+                new ButtonManager("Jabłka", "apples", "textures/items"),
+                new ButtonManager("Bloki", "blocks", "textures/blocks"),
+                new ButtonManager("Łuki", "bows", "textures/items"),
+                new ButtonManager("Wiadra", "buckets", "textures/items"),
+                new ButtonManager("Miecze", "swords", "textures/items"),
+                new ButtonManager("Itemy", "items", "textures/items"),
+                new ButtonManager("Sety", "armors", "textures/models/armor"),
+                new ButtonManager("Gui", "guis", "textures/gui/container"),
+                new ButtonManager("Ikonki", "icons", "textures/gui"),
+                new ButtonManager("Particle", "particles", "textures/particle")
+        };
+
+        Arrays.stream(buttonsTxT).forEach(button -> panels[1].add(button.getButton()));
+        Arrays.stream(panels).forEach(panel -> panel.setLayout(new FlowLayout()));
+
+        panels[0].add(ButtonManager.createTxtButton());
+        panels[0].add(ButtonManager.createShowTextures());
+
+        main.add("Stwórz TXT", panels[0]);
+        main.add("Dodaj tekstury", panels[1]);
+        main.add("Pomoc", panels[2]);
+        panels[2].add(new JLabel("Jeśli potrzebujesz pomocy: https://discord.gg/TMRnDZvmsp"));
+
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                DiscordIntegration.stopRPC();
+                try {
+                    InputStream removeOnline = new URL("https://txtmaker.cf/api/remove-online").openStream();
+                    removeOnline.close();
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+//                    variables.putBoolean("isConnectionDisabled", true);
+                }
+
+            }
+        });
+
+
+        frame.add(main);
+        frame.setVisible(true);
+
+//        if (variables.getBoolean("isConnectionDisabled", false)) {
+//            InputStream removeOnline = new URL("https://txtmaker.cf/api/remove-online").openStream();
+//            removeOnline.close();
+//        }
+
+        new Updater();
+        InputStream addOnline = new URL("https://txtmaker.cf/api/add-online").openStream();
+        addOnline.close();
+
+
+        //aktywowanie statusu na discordzie
+        DiscordIntegration.startRPC();
+
+    }
+
+    public static JFrame getFrame() {
+        return frame;
+    }
+
+    public static ButtonManager[] getButtonsTxT() {
+        return buttonsTxT;
+    }
+}
