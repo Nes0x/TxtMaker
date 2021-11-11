d0bbe43a4cb07911071c82c68b1ff01daa2ccbf6
Nes0x <66802265+Nes0x@users.noreply.github.com>
2021-11-11 15:45:21 +0100

    Add files via upload

--- src/main/java/pl/nesox/TxtMaker.java
+++ src/main/java/pl/nesox/TxtMaker.java
@@ -1,18 +1,21 @@
 package pl.nesox;
 
 import com.github.weisj.darklaf.LafManager;
+import com.github.weisj.darklaf.theme.HighContrastLightTheme;
 import com.github.weisj.darklaf.theme.OneDarkTheme;
+import com.github.weisj.darklaf.theme.SolarizedLightTheme;
+import net.lingala.zip4j.exception.ZipException;
 import pl.nesox.utils.ButtonManager;
 import pl.nesox.utils.DiscordIntegration;
 import pl.nesox.utils.Updater;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.prefs.Preferences;
 
@@ -36,8 +39,13 @@ public class TxtMaker {
     }
 
 
-    public static void main(String[] args) throws IOException {
-        LafManager.install(new OneDarkTheme());
+    public static void main(String[] args) throws IOException, ZipException {
+        if (variables.getBoolean("darkMode", true)) {
+            LafManager.install(new OneDarkTheme());
+        } else {
+            LafManager.install(new HighContrastLightTheme());
+        }
+
 
 
         //tworzenie okna, zakładek, przycisków
@@ -45,7 +53,7 @@ public class TxtMaker {
 
         JTabbedPane main = new JTabbedPane();
 
-        JPanel[] panels = new JPanel[]{new JPanel(), new JPanel(), new JPanel()};
+        JPanel[] panels = new JPanel[]{new JPanel(), new JPanel(), new JPanel(), new JPanel()};
         buttonsTxT = new ButtonManager[]{
                 new ButtonManager("Jabłka", "apples", "textures/items"),
                 new ButtonManager("Bloki", "blocks", "textures/blocks"),
@@ -56,6 +64,7 @@ public class TxtMaker {
                 new ButtonManager("Sety", "armors", "textures/models/armor"),
                 new ButtonManager("Gui", "guis", "textures/gui/container"),
                 new ButtonManager("Ikonki", "icons", "textures/gui"),
+                new ButtonManager("Particle", "particles", "textures/particle")
         };
 
         Arrays.stream(buttonsTxT).forEach(button -> panels[1].add(button.getButton()));
@@ -66,20 +75,49 @@ public class TxtMaker {
 
         main.add("Stwórz TXT", panels[0]);
         main.add("Dodaj tekstury", panels[1]);
-        main.add("Pomoc", panels[2]);
-        panels[2].add(new JLabel("Jeśli potrzebujesz pomocy: https://discord.gg/TMRnDZvmsp"));
+        main.add("Zmiany", panels[2]);
+        main.add("Pomoc", panels[3]);
+
+        JCheckBox enableDarkMode = new JCheckBox("Tryb ciemny");
+        if (variables.getBoolean("darkMode", true)) {
+            enableDarkMode.setSelected(true);
+        }
+
+        enableDarkMode.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                switch (e.getStateChange()) {
+                    case ItemEvent.SELECTED:
+                        variables.putBoolean("darkMode", true);
+                        LafManager.install(new OneDarkTheme());
+                        break;
+                    case ItemEvent.DESELECTED:
+                        variables.putBoolean("darkMode", false);
+                        LafManager.install(new HighContrastLightTheme());
+                        break;
+                }
+            }
+        });
+
+        JTextArea changelog = new JTextArea();
+        changelog.setText("+ Automatyczne aktualizacje \n+ Zakładka zmiany \n+ Wybór motywu");
+        changelog.setEditable(false);
+        JScrollPane changelogPane = new JScrollPane(changelog);
+
+        panels[2].add(changelogPane);
+
+        JTextArea help = new JTextArea("Jeśli potrzebujesz pomocy wejdz na discorda: \nhttps://discord.gg/ZwKVn2XbWc");
+        help.setEditable(false);
+
+        panels[3].add(new JScrollPane(help));
+        panels[3].add(enableDarkMode);
+
 
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent windowEvent) {
                 DiscordIntegration.stopRPC();
-                try {
-                    InputStream removeOnline = new URL("https://txtmaker.cf/api/remove-online").openStream();
-                    removeOnline.close();
-                } catch (IOException exception) {
-                    exception.printStackTrace();
-                }
 
             }
         });
@@ -88,9 +126,9 @@ public class TxtMaker {
         frame.add(main);
         frame.setVisible(true);
 
+
+        //Sprawdzanie aktualizacji
         new Updater();
-        InputStream addOnline = new URL("https://txtmaker.cf/api/add-online").openStream();
-        addOnline.close();
 
 
         //aktywowanie statusu na discordzie
